5f94d3f7d0d58af8380f6b32e4bc94b88531a9c7
Vatslav <rvatslav@gmail.com>
2026-08-21 21:08:10 +0500

    add app and category models

--- main/views.py
+++ main/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import  HttpResponse
+from .models import App
 
+res=''
+for i in App.objects.all():
+    res += f'{i.name},'
 
 def index(request):
-    return HttpResponse('index here')
+    return HttpResponse(f'Приложений в магазине: {res}')
+
