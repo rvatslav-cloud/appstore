7db3f4b7e15dc6505f398bacd6bac19dc85d0ee5
Vatslav <rvatslav@gmail.com>
2026-08-31 17:21:55 +0500

    app detail

--- main/views.py
+++ main/views.py
@@ -1,13 +1,29 @@
-from django.shortcuts import render
-
-from .models import App
+from django.shortcuts import render, get_object_or_404
+from django.http import  HttpResponse
+from .models import App, Category
 
 
 def index(request):
     apps = App.objects.order_by('name').all()
     featured = App.objects.order_by('-price').first()
-    return render(request, 'main/index.html', {'apps': apps})
+    return render(request, 'main/index.html', {
+        'apps': apps,
+        'featured' : featured,
+    })
 
 
 def about(request):
     return render(request, 'main/about.html')
+
+
+def app_detail(request,app_id):
+    app =get_object_or_404(App, id = app_id)
+    return render(request, 'main/app_detail.html',{'app': app})
+
+def category_detail(request,category_id):
+    category = get_object_or_404(Category,id = category_id)
+    apps = App.objects.filter(category=category),
+    return render(request, 'main/category.html',{
+        'category': category,
+        'apps': apps,
+    })
