d22cc169370882a34d97451ef41e8dea3563a182
Vatslav <rvatslav@gmail.com>
2026-08-31 23:28:12 +0500

    category page

--- main/views.py
+++ main/views.py
@@ -4,11 +4,13 @@ from .models import App, Category
 
 
 def index(request):
-    apps = App.objects.order_by('name').all()
+    apps = App.objects.order_by('-created_at').all()
     featured = App.objects.order_by('-price').first()
+    categories = Category.objects.all()
     return render(request, 'main/index.html', {
         'apps': apps,
         'featured' : featured,
+        'categories' : categories,
     })
 
 
@@ -22,7 +24,7 @@ def app_detail(request,app_id):
 
 def category_detail(request,category_id):
     category = get_object_or_404(Category,id = category_id)
-    apps = App.objects.filter(category=category),
+    apps = App.objects.filter(category = category)
     return render(request, 'main/category.html',{
         'category': category,
         'apps': apps,
