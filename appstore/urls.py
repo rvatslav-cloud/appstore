56925d980e1de5e2ba61ff61a1d6f80325d2f1a6
Vatslav <rvatslav@gmail.com>
2026-09-21 21:53:32 +0500

    practice. Task 1 done.

--- appstore/urls.py
+++ appstore/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path('', include('main.urls', namespace='main')),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL,
+document_root=settings.MEDIA_ROOT)
